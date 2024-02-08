a8e472053f43d8f96957a7f4b99bacc9d15ec598
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-08 23:54:13 +0100

    get logger update more

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -37,7 +37,7 @@ def filter_datum(fields: List[str], redaction: str,
 
 def get_logger() -> logging.Logger:
     """ Return logger """
-    logger = logging.getLogger()
+    logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
     handler = logging.StreamHandler()
