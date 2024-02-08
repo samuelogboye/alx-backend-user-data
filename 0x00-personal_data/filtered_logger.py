2d456de25fda07c21e55a898fe883e1e51dea266
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-08 23:00:47 +0100

    filtered logger more

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,14 +5,9 @@ Filtered Logger
 import re
 
 
-def filter_datum(
-        fields: list,
-        redaction: str,
-        message: str,
-        separator: str) -> str:
-    """
-    Filter the message
-    """
+def filter_datum(fields: list, redaction: str, message: str,
+                 separator: str) -> str:
+    """ Filter the message """
     return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
                   lambda match: match.group(1) + "=" + redaction
                   if match.group(1) in fields else match.group(0), message)
