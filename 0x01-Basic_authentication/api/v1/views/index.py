6879d856daff45b378978d0790269d0ea9dceb4d
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-12 23:30:47 +0100

    handle error forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - 401
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - 403
+    """
+    abort(403)
