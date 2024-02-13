2d68385f8a3cb26240bc7f4cfbc3edbeb917661c
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-13 21:47:42 +0100

    basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,30 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+if os.getenv("AUTH_TYPE") == "auth":
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+# elif os.getenv("AUTH_TYPE") == "basic_auth":
+#         from api.v1.auth.basic_auth import BasicAuth
+#         auth = BasicAuth()
+
+
+@app.before_request
+def before_request() -> str:
+    """ Before request handler
+    """
+    paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+             '/api/v1/forbidden/']
+    if auth is None:
+        return
+    if auth.require_auth(request.path, paths) is False:
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
