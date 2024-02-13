9e8b0d477c00ac074c10cdfe8d49a30aada28cbc
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-13 21:35:29 +0100

    authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Auth module
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require auth"""
+        if path is None or excluded_paths is None:
+            return True
+        if path in excluded_paths:
+            return False
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Authorization header"""
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current user"""
+        return None
