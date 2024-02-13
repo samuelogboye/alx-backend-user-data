b6695c5169acc5de3f0241599f1ca138180b1c97
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-13 22:35:13 +0100

    encode and decode authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,3 +30,42 @@ class BasicAuth(Auth):
         try:
             return base64_authorization_header.encode('utf-8').decode('utf-8')
         except Exception:
+            return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Extract user credentials
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        credentials = decoded_base64_authorization_header.split(':', 1)
+        return credentials[0], credentials[1]
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> dict:
+        """ User object from credentials
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current user
+        """
+        try:
+            auth_header = self.authorization_header(request)
+            base64_header = self.extract_base64_authorization_header(
+                auth_header)
+            decoded_base64_header = self.decode_base64_authorization_header(
+                base64_header)
+            user_email, user_pwd = self.extract_user_credentials(
+                decoded_base64_header)
+            user = self.user_object_from_credentials(user_email, user_pwd)
+            return user
+        except Exception:
+            return None
