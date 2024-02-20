123bb82fb8723be66c44f3d315e5aa7c828f810b
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-20 22:27:53 +0100

    db model updated

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,6 +33,8 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add user
         """
+        if not email or not hashed_password:
+            return
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
