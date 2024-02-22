85308deaeaa9a85b8376827ce57429645ae74473
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-23 00:39:05 +0100

    more login more ok ok yes

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,7 +88,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(id=user_id)
-            self._db.update_user(user.id, session_id=None)
+            user.session_id = None
             return None
         except NoResultFound:
             return None
