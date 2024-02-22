9bd5c5a6b264ee3dab9b477a2099a35bb0632719
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-23 00:35:36 +0100

    more login more

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,7 +4,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
