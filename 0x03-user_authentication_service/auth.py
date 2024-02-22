36744d3f51cf5ba336f4a916e971227401322546
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-23 00:06:55 +0100

    modified

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -49,7 +49,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return checkpw(str.encode(password), user.hashed_password)
+            return checkpw(password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
 
