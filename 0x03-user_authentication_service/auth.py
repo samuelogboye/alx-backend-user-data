21cd9a246c2050a1210e01f400370debe45777f2
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-23 00:40:21 +0100

    more login hmm

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -107,9 +107,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = _generate_uuid()
-            self._db.update_user(user.id, reset_token=reset_token)
-            return reset_token
+            user.reset_token = _generate_uuid()
+            return user.reset_token
         except NoResultFound:
             raise ValueError
 
